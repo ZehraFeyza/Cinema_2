6514e75bed058307de4a09f468f960d773b49a0c
ZehraFeyza <zehracicek0571@gmail.com>
2022-03-01 10:15:44 -0800

    v7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import CustomRoutes from "./router/CustomRoute";
+import CustomRoutes from "./router/CustomRoutes";
 
 const App = () => {
   return (
